69e534096a3efef1386aad76edfc319c277b41a0
mouna07 <mounamc261@gmail.com>
2021-02-24 20:15:15 +0530

    ADDED: code for special rates for reward customers

--- src/main/java/com/BridgeLabz/HotelReservation/HotelReservation.java
+++ src/main/java/com/BridgeLabz/HotelReservation/HotelReservation.java
@@ -14,6 +14,7 @@ public class HotelReservation {
     long weekDays;
     long weekendDays;
     int rating;
+
     public HotelReservation() {
         hotelList = new ArrayList<>();
     }
@@ -23,9 +24,9 @@ public class HotelReservation {
     }
 
     public void addHotelDetails() {
-        hotelList.add(new Hotel("LakeWood", 3, 110, 90));
-        hotelList.add(new Hotel("BridgeWood", 4, 150, 50));
-        hotelList.add(new Hotel("RidgeWood", 5, 220, 150));
+        hotelList.add(new Hotel("LakeWood", 3, 110, 90, 80, 80));
+        hotelList.add(new Hotel("BridgeWood", 4, 150, 50, 110, 50));
+        hotelList.add(new Hotel("RidgeWood", 5, 220, 150, 100, 40));
     }
 
     /* count week days */
@@ -42,10 +43,78 @@ public class HotelReservation {
         return count;
     }
 
+    /* Find hotel by maximum ratings */
+    public String calculateCheapestHotelAndRateByRating(String dateOfArrival, String dateOfDeparture) {
+        LocalDate dateArrival = LocalDate.parse(dateOfArrival);
+        LocalDate dateDeparture = LocalDate.parse(dateOfDeparture);
+        totalDays = ChronoUnit.DAYS.between(dateArrival, dateDeparture) + 1;
+        weekDays = countWeekDaysMath(dateArrival, dateDeparture);
+        weekendDays = totalDays - weekDays;
+        long cost = 0;
+        rating = 0;
+        String hotelName = null;
+        minCostLakeWood = (hotelList.get(0).getRegularRate() * weekDays
+                + hotelList.get(0).getWeekendRate() * weekendDays);
+        minCostBridgeWood = (hotelList.get(1).getRegularRate() * weekDays
+                + hotelList.get(1).getWeekendRate() * weekendDays);
+        minCostRidgeWood = (hotelList.get(2).getRegularRate() * weekDays
+                + hotelList.get(2).getWeekendRate() * weekendDays);
+        //long minCostHotel = Math.min(minCostRidgeWood, Math.min(minCostBridgeWood, minCostLakeWood));
+        if ((hotelList.get(2).getHotelRating() > rating)) {
+            hotelName = hotelList.get(2).getHotelName();
+            cost = minCostRidgeWood;
+            rating = hotelList.get(2).getHotelRating();
+        }
+        if ((hotelList.get(1).getHotelRating() > rating)) {
+            hotelName = hotelList.get(1).getHotelName();
+            cost = minCostBridgeWood;
+            rating = hotelList.get(1).getHotelRating();
+        }
+        if ((hotelList.get(0).getHotelRating() > rating)) {
+            hotelName = hotelList.get(0).getHotelName();
+            cost = minCostLakeWood;
+            rating = hotelList.get(0).getHotelRating();
+        }
+        System.out.println(
+                "The minimum cost for Hotel " + hotelName + " is " + cost + " and rating of hotel is " + rating);
+        return hotelName;
+    }
+
     /* Find cheapest hotel */
-    public ArrayList<String> findCheapestCost(long weekDays, long weekendDays) {
+    public ArrayList<String> calculateCheapestHotelAndRate(String dateOfArrival, String dateOfDeparture) {
+        LocalDate dateArrival = LocalDate.parse(dateOfArrival);
+        LocalDate dateDeparture = LocalDate.parse(dateOfDeparture);
+        totalDays = ChronoUnit.DAYS.between(dateArrival, dateDeparture) + 1;
+        weekDays = countWeekDaysMath(dateArrival, dateDeparture);
+        weekendDays = totalDays - weekDays;
         ArrayList<String> hotel = new ArrayList<>();
-        minCostLakeWood = 0;long cost = 0; minCostBridgeWood = 0; minCostRidgeWood = 0; rating = 0; String hotelName = null;
+        long minCostLakeWood = (hotelList.get(0).getRegularRate() * weekDays
+                + hotelList.get(0).getWeekendRate() * weekendDays);
+        long minCostBridgeWood = (hotelList.get(1).getRegularRate() * weekDays
+                + hotelList.get(1).getWeekendRate() * weekendDays);
+        long minCostRidgeWood = (hotelList.get(2).getRegularRate() * weekDays
+                + hotelList.get(2).getWeekendRate() * weekendDays);
+        long minCostHotel = Math.min(minCostRidgeWood, Math.min(minCostBridgeWood, minCostLakeWood));
+        if (minCostHotel == minCostLakeWood)
+            hotel.add("LakeWood");
+        if (minCostHotel == minCostBridgeWood)
+            hotel.add("BridgeWood");
+        if (minCostHotel == minCostRidgeWood)
+            hotel.add("RidgeWood");
+        System.out.println("The minimum cost for Hotel is " + minCostHotel);
+        return hotel;
+    }
+
+    /* UC6 Find cheapest hotel with maximum rating */
+
+    public String findCheapestCostWithMaxRatings(String dateOfArrival, String dateOfDeparture) {
+        int rating = 0;
+        LocalDate dateArrival = LocalDate.parse(dateOfArrival);
+        LocalDate dateDeparture = LocalDate.parse(dateOfDeparture);
+        totalDays = ChronoUnit.DAYS.between(dateArrival, dateDeparture) + 1;
+        weekDays = countWeekDaysMath(dateArrival, dateDeparture);
+        weekendDays = totalDays - weekDays;
+        String hotelName = null;
         minCostLakeWood = (hotelList.get(0).getRegularRate() * weekDays
                 + hotelList.get(0).getWeekendRate() * weekendDays);
         minCostBridgeWood = (hotelList.get(1).getRegularRate() * weekDays
@@ -53,37 +122,21 @@ public class HotelReservation {
         minCostRidgeWood = (hotelList.get(2).getRegularRate() * weekDays
                 + hotelList.get(2).getWeekendRate() * weekendDays);
         long minCostHotel = Math.min(minCostRidgeWood, Math.min(minCostBridgeWood, minCostLakeWood));
-        if ((hotelList.get(2).getHotelRating() > rating)) {
-            hotelName = hotelList.get(2).getHotelName();
-            cost = minCostRidgeWood;
+        if (minCostHotel == minCostRidgeWood && (hotelList.get(2).getHotelRating() > rating)) {
             rating = hotelList.get(2).getHotelRating();
-            hotel.add(hotelName);
+            hotelName = hotelList.get(2).getHotelName();
         }
-        if ((hotelList.get(1).getHotelRating() > rating)) {
-            hotelName = hotelList.get(1).getHotelName();
-            cost = minCostBridgeWood;
+        if (minCostHotel == minCostBridgeWood && (hotelList.get(1).getHotelRating() > rating)) {
             rating = hotelList.get(1).getHotelRating();
-            hotel.add(hotelName);
+            hotelName = hotelList.get(1).getHotelName();
         }
-        if ((hotelList.get(0).getHotelRating() > rating)) {
-            hotelName = hotelList.get(0).getHotelName();
-            cost = minCostLakeWood;
+        if (minCostHotel == minCostLakeWood && (hotelList.get(0).getHotelRating() > rating)) {
             rating = hotelList.get(0).getHotelRating();
-            hotel.add(hotelName);
+            hotelName = hotelList.get(0).getHotelName();
         }
-        System.out.println("The minimum cost for Hotel "+hotelName+" is " + cost + " and rating of hotel is " + rating);
-        return hotel;
-    }
-
-    // Calculated rate based on period of stay at hotel
-    public ArrayList<String> calculateCheapestHotelAndRate(String dateOfArrival, String dateOfDeparture)throws Exception {
-        totalDays = 0; weekDays = 0; weekendDays = 0;
-        LocalDate dateArrival = LocalDate.parse(dateOfArrival);
-        LocalDate dateDeparture = LocalDate.parse(dateOfDeparture);
-        totalDays = ChronoUnit.DAYS.between(dateArrival, dateDeparture) + 1;
-        weekDays = countWeekDaysMath(dateArrival, dateDeparture);
-        weekendDays = totalDays - weekDays;
-        return findCheapestCost(weekDays, weekendDays);
+        System.out.println("The minimum cost for Hotel " + hotelName + " is " + minCostHotel
+                + " and rating of hotel is " + rating);
+        return hotelName;
     }
 
 }
