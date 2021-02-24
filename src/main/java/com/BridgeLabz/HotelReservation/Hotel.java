634a7a32c6316388f6a685807c7d44e249db0798
mouna07 <mounamc261@gmail.com>
2021-02-24 20:02:32 +0530

    ADDED: code for Adding Rates to each hotel

--- src/main/java/com/BridgeLabz/HotelReservation/Hotel.java
+++ src/main/java/com/BridgeLabz/HotelReservation/Hotel.java
@@ -3,12 +3,14 @@ public class Hotel {
 
     // Parameters
     private String hotelName;
+    private int hotelRating;
     private int regularRate;
     private int weekendRate;
 
-    //Parameterized Constructor
-    public Hotel(String hotelName, int regularRate, int weekendRate) {
+    // Parameterized Constructor
+    public Hotel(String hotelName, int hotelRating, int regularRate, int weekendRate) {
         this.hotelName = hotelName;
+        this.hotelRating = hotelRating;
         this.regularRate = regularRate;
         this.weekendRate = weekendRate;
     }
@@ -36,5 +38,13 @@ public class Hotel {
 
     public void setWeekendRate(int weekendRate) {
         this.weekendRate = weekendRate;
+    }
+
+    public int getHotelRating() {
+        return hotelRating;
+    }
+
+    public void setHotelRating(int hotelRating) {
+        this.hotelRating = hotelRating;
     }
 }
