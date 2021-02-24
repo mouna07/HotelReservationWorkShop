282d8352b0ebb031dd056fc83361ea50e0a4f754
mouna07 <mounamc261@gmail.com>
2021-02-24 19:56:05 +0530

    ADDED: code for adding weekdays and weekend days

--- src/test/java/com/BridgeLabz/HotelReservation/HotelReservationTest.java
+++ src/test/java/com/BridgeLabz/HotelReservation/HotelReservationTest.java
@@ -1,28 +1,31 @@
 package com.BridgeLabz.HotelReservation;
 
+import java.util.ArrayList;
+
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class HotelReservationTest
-{
+public class HotelReservationTest {
     static HotelReservation hotelReservation;
+
     @BeforeClass
     public static void initialize() {
         hotelReservation = new HotelReservation();
     }
 
-    @Test
-    public void shouldPrintWelcomeMessage()  {
-        HotelReservation hotelReservation = new HotelReservation();
+    @Before
+    public void shouldPrintWelcomeMessage() {
         hotelReservation.printWelcomeMessage();
     }
 
     @Test
-    public void givenDate_shouldReturnLakeWood()throws Exception {
-        HotelReservation hotelReservation = new HotelReservation();
+    public void givenDate_shouldReturn_CheapestHotelNameAndRate() throws Exception {
         hotelReservation.addHotelDetails();
-        String cheapestHotel = hotelReservation.calculateCheapestHotelAndRate("10Sep2020", "11Sep2020");
-        Assert.assertEquals("Lakewood", cheapestHotel);
+        ArrayList<String> hotelNameList = hotelReservation.calculateCheapestHotelAndRate("2020-09-10", "2020-09-11");
+        Object[] hotelName = hotelNameList.toArray();
+        Object[] arrayExpectedOutput = { "LakeWood", "BridgeWood" };
+        Assert.assertArrayEquals(arrayExpectedOutput, hotelName);
     }
 }
